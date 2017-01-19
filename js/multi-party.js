5cb47b5a280237f2d45586cea07bbc885a70726e
Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>
2017-01-20 03:47:50 +0530

    Break down main client side script into functions
    Signed-off-by: Kaustav Das Modak <kaustav.dasmodak@yahoo.co.in>

--- js/multi-party.js
+++ js/multi-party.js
@@ -16,9 +16,20 @@ window.addEventListener("load", function () {
         };
     };
 
+    var subscribe_event_handler = function (event) {
+        console.log("Subscriber event", event);
+    };
+
     var subscribe = function (stream_name) {
+        var subscribers_el = document.getElementById("subscribers");
+        var vid = document.createElement("video");
+        vid.id = "subscriber-" + stream_name;
+        subscribers_el.appendChild(vid);
+
         var subscriber = new red5prosdk.RTCSubscriber();
-        var subscriber_view = new red5prosdk.Red5ProPlaybackView();
+        subscriber.on("*", subscribe_event_handler);
+
+        var subscriber_view = new red5prosdk.PlaybackView("subscriber-" + stream_name);
         subscriber_view.attachSubscriber(subscriber);
 
         subscriber.init(get_params(stream_name))
@@ -33,18 +44,31 @@ window.addEventListener("load", function () {
             });
     };
 
-    var publish = function () {
+    var publish_event_handler = function (event) {
+        console.log("Publisher event", event);
+    };
+
+    var create_publisher = function () {
         // Create publisher
         var publisher = new red5prosdk.RTCPublisher();
+        publisher.on("*", publish_event_handler);
+        return publisher;
+    };
+
+    var create_publisher_view = function (publisher) {
         var publisher_view = new red5prosdk.PublisherView("publisher-view");
         publisher_view.attachPublisher(publisher);
+        return publisher_view;
+    };
 
+    var publish = function (publisher, publisher_view) {
         // Using Chrome/Google TURN/STUN servers.
         var iceServers = [{urls: "stun:stun2.l.google.com:19302"}];
+        var params = get_params();
 
         // Initialize
         publisher
-            .init(get_params())
+            .init(params)
             .then(function() {
                 // Invoke the publish action
                 return publisher.publish();
@@ -62,6 +86,9 @@ window.addEventListener("load", function () {
             video: true
         }, function(media) {
 
+            var publisher = create_publisher();
+            var publisher_view = create_publisher_view(publisher);
+
             // Upon access of user media,
             // 1. Attach the stream to the publisher.
             // 2. Show the stream as preview in view instance.
@@ -69,7 +96,8 @@ window.addEventListener("load", function () {
             publisher_view.preview(media);
 
             // Try publishing
-            publish();
+            publish(publisher, publisher_view);
+            subscribe();
         }, function(error) {
             console.error(error);
         });
